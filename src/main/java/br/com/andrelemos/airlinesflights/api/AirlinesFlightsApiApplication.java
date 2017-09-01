6ec6b2a40260338764b785850bc43f1f0298a551
Andre Lemos <andre.lemos@reservafacil.tur.br>
2017-09-01 13:03:18 -0300

    Corrige imports.

--- src/main/java/br/com/andrelemos/airlinesflights/api/AirlinesFlightsApiApplication.java
+++ src/main/java/br/com/andrelemos/airlinesflights/api/AirlinesFlightsApiApplication.java
@@ -2,12 +2,8 @@ package br.com.andrelemos.airlinesflights.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-
-import br.com.andrelemos.airlinesflights.api.config.AirlinesFlightsApiProperties;
 
 @SpringBootApplication
-//@EnableConfigurationProperties(AirlinesFlightsApiProperties.class)
 public class AirlinesFlightsApiApplication {
 
 	public static void main(String[] args) {
